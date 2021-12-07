7f9fd3bcea8f147b06b4c61ac597a9fb2eb1ccd7
Yan San <puyan.san@ooni.com>
2021-12-07 15:07:27 +0000

    Refactor of day 1

--- day1/part2.js
+++ day1/part2.js
@@ -1,44 +1,23 @@
-//0 199  A
-//1 200  A B
-//2 208  A B C
-//3 210    B C D
-//4 200  E   C D
-//5 207  E F   D
-//6 240  E F G
-//7 269    F G H
-//8 260      G H
-//9 263        H
-
-
-// Start by comparing the first and second three-measurement windows.
-// The measurements in the first window are marked A (199, 200, 208);
-// their sum is 199 + 200 + 208 = 607.
-
-// The second window is marked B (200, 208, 210);
-// its sum is 618.
-
-// The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
-
 const { input } = require('./inputParser');
 
 function loopMeasurements() {
-  let previousSum;
   let incrementCount = 0;
-  input.map((num, index, arr) => {
-    const sumOfA = sumOfWindow(arr, index);
-    const sumOfB = sumOfWindow(arr, index+1);
+
+  for(i = 0; i < input.length; i++) {
+    const sumOfA = sumOfWindow(i);
+    const sumOfB = sumOfWindow(i+1);
 
     if (sumOfA > 0 && sumOfB > 0 && sumOfB > sumOfA) {
       incrementCount++;
     }
-  });
+  }
 
   console.log(incrementCount);
 }
 
-function sumOfWindow(arr, index) {
-  if (arr[index] && arr[index+1] && arr[index+2]) {
-    return arr[index] + arr[index+1] + arr[index+2];
+function sumOfWindow(index) {
+  if (input[index] && input[index+1] && input[index+2]) {
+    return input[index] + input[index+1] + input[index+2];
   }
 
   return null;
